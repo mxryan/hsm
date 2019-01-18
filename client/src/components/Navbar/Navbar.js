0b26bd3f66a9e935576a8eadb650998d5acfbf02
M Ryan <mxryan@live.com>
2019-01-18 12:43:59 -0500

    initial skeleton

---
+++ client/src/components/Navbar/Navbar.js
@@ -0,0 +1,12 @@
+import React from "react";
+const Navbar = () => {
+  return (
+    <div>
+      <h5>Test</h5>
+      <p>If you see this then everything is okey-dokey</p>
+    
+    </div>
+  )
+}
+
+export default Navbar;
---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+const PORT = 8080;
+
+app.use(express.static(path.join(__dirname, "client/build")));
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname + "/client/build/index.html"));
+})
+
+
+app.listen(PORT, ()=> console.log("server on"));
