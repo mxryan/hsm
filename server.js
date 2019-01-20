5c294e392289fa0ade16592ecdc88399310a1a33
M Ryan <mxryan@live.com>
2019-01-20 07:57:32 -0500

    added pkg json

--- server.js
+++ server.js
@@ -3,6 +3,12 @@ const path = require("path");
 const app = express();
 const PORT = 8080;
 
+// to do:
+// init sequelize
+// db set up
+// auth set up
+// route set up
+
 app.use(express.static(path.join(__dirname, "client/build")));
 
 app.get("/", (req, res) => {
